c9911e9bddfe427f1eafb496daf625f7a40e343c
Jai Luthra <me@jailuthra.in>
2013-04-06 15:50:21 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+##Rock-Paper-Scissors In Ruby
+
+It is a classic Rock Paper Scissors game written in ruby!
+
+To play:
+```$ chmod +x rock-paper-scissors.rb
+$ ./rock-paper-scissors```
---
+++ rock-paper-scissors.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+
+class Game
+  
+  def take_user_input
+    print "\nIt's your turn, play something (rock/paper/scissors): "
+    @user_choice = gets.chomp.downcase
+  end
+  
+  def take_cpu_input
+    choices = ['rock', 'paper', 'scissors']
+    @cpu_choice = choices[rand(3)]
+  end
+
+  def winner?
+    winner = nil
+    
+    if @user_choice == 'rock'
+      return winner = 'user' if @cpu_choice == 'scissors'
+      return winner = 'cpu' if @cpu_choice == 'paper'      
+    elsif @user_choice == 'paper'
+      return winner = 'user' if @cpu_choice == 'rock'
+      return winner = 'cpu' if @cpu_choice == 'scissors'
+    elsif @user_choice == 'scissors'
+      return winner = 'user' if @cpu_choice == 'paper'
+      return winner = 'cpu' if @cpu_choice == 'rock'
+    else raise ArgumentError, "Choose something from ['rock', 'paper', 'scissors']"
+    end
+  end
+  
+  def play
+    self.take_user_input
+    self.take_cpu_input
+    puts "\nYou chose #{@user_choice}."
+    puts "I chose #{@cpu_choice}."
+    winner = self.winner?
+    
+    # Printing the winner
+    puts "\nI won, a puny human couldn't beat me!" if winner == 'cpu'
+    puts "\nCongrats fleshbag, you shall return victorious!" if winner == 'user'
+    puts "\nGAME TIED: Triumph said 'NO' to both." if winner == nil
+  end
+
+end
+
+game = Game.new
+game.play
